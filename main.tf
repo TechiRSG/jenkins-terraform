123e8ac41a21877966d4eeb5b3c2770657ef5ef3
suryatejayalla <88100093+suryatejayalla@users.noreply.github.com>
2021-12-27 21:49:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_availability_zones" "available" {
 }
 
 resource "aws_vpc" "vpc1" {
-  cidr_block       = 10.0.0.0/16
+  cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   
