690bb5acf23fb3a8c00eb1dbf30dedaf71888f57
suryatejayalla <88100093+suryatejayalla@users.noreply.github.com>
2021-12-28 12:18:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "suryatf"
-    key    = "network/infra-terraform.tfstate"
-    region = "ap-south-1"
+    key    = "infra-terraform/"
+    region = "us-east-2"
   }
 }
 
